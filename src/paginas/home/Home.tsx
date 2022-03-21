2143fa44883cbdb1577e72fcb79a857d400e5abe
Laura Barros Souza <laurabarros2541@gmail.com>
2022-03-21 17:36:54 -0300

    Initialize project using Create React App

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://imgur.com/0vUMmZh.png" alt="imagem de praia" className="img"/>
+        </>
+    );
+}
+
+export default Home;
