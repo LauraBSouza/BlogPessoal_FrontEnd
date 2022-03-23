6b3ab8d9d557bbffac41b4ca0751f3d917ad8958
Laura Barros Souza <laurabarros2541@gmail.com>
2022-03-23 20:30:13 -0300

    Estilizando o Navbar com css

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,45 +1,47 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 
+import './Navbar.css'
+
 function Navbar() {
     return (
         <>
-            <AppBar position="static">
-                <Toolbar variant="dense">
+            <AppBar position="static" className='back'>
+                <Toolbar variant="dense" className='bar'>
                     <Box style={{ cursor: "pointer" }} >
-                        <Typography variant="h5" color="inherit">
+                        <Typography variant="h5" color="inherit" className='title'>
                             BlogPessoal
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
                       <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit">
+                          <Typography variant="h6" color="inherit" className='text'>
                             home
                           </Typography>
                         </Box>
                     </Box>
                     
                       <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit">
+                          <Typography variant="h6" color="inherit" className='text'>
                             postagens
                           </Typography>
                         </Box>
 
                       <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit">
+                          <Typography variant="h6" color="inherit" className='text'>
                             temas
                           </Typography>
                         </Box>
 
                       <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit">
+                          <Typography variant="h6" color="inherit" className='text'>
                             cadastrar tema
                           </Typography>
                         </Box>
                 
                       <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit">
+                          <Typography variant="h6" color="inherit" className='text'>
                             logout
                           </Typography>
                         </Box>
