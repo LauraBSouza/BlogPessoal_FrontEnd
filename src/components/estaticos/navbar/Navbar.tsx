e12da3f5745b17a0c23ccbc953f7e45765851915
Laura Barros Souza <laurabarros2541@gmail.com>
2022-03-24 16:42:53 -0300

    Trabalhando com rotas

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,56 +1,58 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
-
+import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 function Navbar() {
-    return (
-        <>
-            <AppBar position="static" className='back'>
-                <Toolbar variant="dense" className='bar'>
-                    <Box style={{ cursor: "pointer" }} >
-                        <Typography variant="h5" color="inherit" className='title'>
-                            BlogPessoal
-                        </Typography>
-                    </Box>
+  return (
+    <>
+      <AppBar position="static" className='back'>
+        <Toolbar variant="dense" className='bar'>
+          <Box style={{ cursor: "pointer" }} >
+            <Typography variant="h5" color="inherit" className='title'>
+              BlogPessoal
+            </Typography>
+          </Box>
 
-                    <Box display="flex" justifyContent="start">
-                      <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit" className='text'>
-                            home
-                          </Typography>
-                        </Box>
-                    </Box>
-                    
-                      <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit" className='text'>
-                            postagens
-                          </Typography>
-                        </Box>
+          <Box display="flex" justifyContent="start">
+            <Box mx={1} style={{ cursor: "pointer" }} >
+              <Typography variant="h6" color="inherit" className='text'>
+                home
+              </Typography>
+            </Box>
+          </Box>
 
-                      <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit" className='text'>
-                            temas
-                          </Typography>
-                        </Box>
+          <Box mx={1} style={{ cursor: "pointer" }} >
+            <Typography variant="h6" color="inherit" className='text'>
+              postagens
+            </Typography>
+          </Box>
 
-                      <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit" className='text'>
-                            cadastrar tema
-                          </Typography>
-                        </Box>
-                
-                      <Box mx={1} style={{ cursor: "pointer" }} >
-                          <Typography variant="h6" color="inherit" className='text'>
-                            logout
-                          </Typography>
-                        </Box>
-                    
+          <Box mx={1} style={{ cursor: "pointer" }} >
+            <Typography variant="h6" color="inherit" className='text'>
+              temas
+            </Typography>
+          </Box>
 
-                </Toolbar>
-            </AppBar>
-        </>
-    )
+          <Box mx={1} style={{ cursor: "pointer" }} >
+            <Typography variant="h6" color="inherit" className='text'>
+              cadastrar tema
+            </Typography>
+          </Box>
+          <Link to='/login' className='text-decorator-none'>
+            <Box mx={1} style={{ cursor: "pointer", color: "white" }} >
+              <Typography variant="h6" color="inherit" className='text'>
+                logout
+              </Typography>
+            </Box>
+          </Link>
+
+
+
+        </Toolbar>
+      </AppBar>
+    </>
+  )
 }
 
 export default Navbar;
